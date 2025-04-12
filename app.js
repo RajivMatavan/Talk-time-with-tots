5541ba4a7b2a2618adb7e0239ef68cc9965dae84
RajivMatavan <rajmatavan@gmail.com>
2025-04-12 18:12:19 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+
+import React from "react";
+import { render } from "react-dom";
+
+const App = () => (
+    <div>
+        <h1>TalkTime with Tots!</h1>
+        <p>A fun and interactive way for toddlers to practice English!</p>
+    </div>
+);
+
+render(<App />, document.getElementById("root"));
